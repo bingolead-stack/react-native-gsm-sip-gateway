5fd83d25ee5f118d03121d76a8e2082b3f896fac
bingolead-stack <talentdev423@gmail.com>
2026-01-09 07:10:10 -0800

    remove cxx

--- telon-gateway-app/android/app/.cxx/Debug/3i3ud55i/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "C:/Users/A/.gradle/caches/8.14.3/transforms/0cff2664a39a7954f6be903611a50ce6/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.arm64-v8a/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/A/.gradle/caches/8.14.3/transforms/0cff2664a39a7954f6be903611a50ce6/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- telon-gateway-app/android/app/.cxx/Debug/3i3ud55i/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/A/.gradle/caches/8.14.3/transforms/9ff38684db6fe6acfdda42b443fcf80d/transformed/hermes-android-0.80.0-debug/prefab/modules/libhermes/libs/android.x86/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/A/.gradle/caches/8.14.3/transforms/9ff38684db6fe6acfdda42b443fcf80d/transformed/hermes-android-0.80.0-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
